b96f7483e27d959e4a12adbe4d5ef25161acb365
shao <444395398@qq.com>
2022-07-10 23:18:20 +0800

    版本修改

---
+++ src/ConfigProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace ShiSuKj\JwtAuth;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+            ],
+            'commands' => [
+            ],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+            'publish' => [
+                [
+                    'id' => 'jwt',
+                    'description' => 'hyperf-jwt',
+                    'source' => __DIR__ . '/../publish/jwt.php',
+                    'destination' => BASE_PATH . '/config/autoload/jwt.php',
+                ],
+            ],
+        ];
+    }
+}
